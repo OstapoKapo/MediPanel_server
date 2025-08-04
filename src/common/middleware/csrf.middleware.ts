1d45b13ae45e9393a6af54d62eb6dfe592764dfc
Ostap Prodanyuk <105784216+OstapoKapo@users.noreply.github.com>
2025-08-04 05:03:20 +0300

    feat: add ip and ua to user, closes #13 (#21)
    * feat: add ip and ua to user, closes #13
    
    * fix: codacy eror
    
    * fix: codacy eror

--- src/common/middleware/csrf.middleware.ts
+++ src/common/middleware/csrf.middleware.ts
@@ -2,25 +2,42 @@ import { ForbiddenException, Injectable, NestMiddleware } from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
 import { RedisService } from "src/redis/redis.service";
 
-const csrfWhiteList = ['/auth/logIn', '/auth/verifyPassword', '/auth/logOut'];
+const csrfWhiteList = ['/auth/logIn', '/auth/verifyPassword']
 const csrfWhiteListMethods = ['POST', 'PUT', 'DELETE', 'PATCH'];
 
 @Injectable()
 export class CsrfMiddleware implements NestMiddleware {
     constructor(
-        private readonly redisService: RedisService
+        private  redisService: RedisService
     ){}
 
-    async use(req: Request, res: Response, next: NextFunction){
-        if(csrfWhiteListMethods.includes(req.method) && !csrfWhiteList.includes(req.path)) {
-            const sessionId = req.cookies.sessionId;
-            const csrfHeader = req.headers['x-csrf-token'];
-
-            if(!sessionId || !csrfHeader) throw new ForbiddenException('Mising CSRF token or session id');
-
-            const session = await this.redisService.get<{csrfToken: string}>(`session: ${sessionId}`);
-            if(!session || session.csrfToken !== csrfHeader) throw new ForbiddenException('Invalid CSRF Token');
-        }
-        next();
+    public with(redisService: RedisService): this {
+        this.redisService = redisService;
+        return this;
     }
+
+   async use(req: Request, res: Response, next: NextFunction) {
+  try {
+    if (csrfWhiteListMethods.includes(req.method) && !csrfWhiteList.includes(req.path)) {
+      const sessionId = req.cookies.sessionId;
+      const csrfHeader = req.headers['x-csrf-token'];
+
+      if (!sessionId || !csrfHeader) {
+        throw new ForbiddenException('Missing CSRF token or session id');
+      }
+
+      const session = await this.redisService.get<{ csrfToken: string }>(`session:${sessionId}`);
+      console.log(sessionId, csrfHeader)
+      if (!session || session.csrfToken !== csrfHeader) {
+        throw new ForbiddenException('Invalid CSRF Token');
+      }
+    }
+
+    next();
+  } catch (err) {
+    console.error('CSRF Middleware Error:', err);
+    next(err); 
+  }
+}
+
 }
